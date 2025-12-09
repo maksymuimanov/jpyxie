b1b209cf647ad688f7bc576b04de070aefd8c75a
Maksym Uimanov <uimanovmaks@gmail.com>
2025-12-09 13:30:41 +0100

    Enhance Python date deconversion logic to support additional date formats and improve utility pattern matching.

--- bind/python-executor-bind-core/src/main/java/io/maksymuimanov/python/deconverter/PythonDateDeconverter.java
+++ bind/python-executor-bind-core/src/main/java/io/maksymuimanov/python/deconverter/PythonDateDeconverter.java
@@ -6,14 +6,21 @@ import io.maksymuimanov.python.exception.PythonUnsupportedTypeDeconversionExcept
 import io.maksymuimanov.python.util.StringUtils;
 import org.jspecify.annotations.Nullable;
 
-import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.temporal.ChronoField;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PythonDateDeconverter extends AbstractPythonTypeDeconverter<PythonDate, LocalDate> {
-    public static final int DEFAULT_DATE_PARTS = 3;
+    public static final Map<String, Integer> MONTHS = Arrays.stream(Month.values())
+            .collect(Collectors.toUnmodifiableMap(month -> month.name().substring(0, 3), Month::getValue));
+    public static final Pattern YEAR_PATTERN = Pattern.compile("[0-9]{4}");
+    public static final Pattern MONTH_PATTERN = Pattern.compile("[A-Za-z]{3}");
+    public static final Pattern DAY_PATTERN = Pattern.compile("[0-9]{2}");
+    public static final int TOTAL_MONTHS = 12;
 
     @Override
     @Nullable
@@ -30,76 +37,43 @@ public class PythonDateDeconverter extends AbstractPythonTypeDeconverter<PythonD
     @Override
     public PythonDate resolve(CharSequence value, PythonDeserializer pythonDeserializer) {
         LocalDate result = this.getValue(value, (key) -> {
-            List<Integer> dateParts = new ArrayList<>(DEFAULT_DATE_PARTS);
-            int current = 0;
-            int base = 1;
-            boolean isReading = true;
-            for (int i = key.length() - 1; i >= 0; i--) {
-                char aChar = key.charAt(i);
-                boolean isDigit = Character.isDigit(aChar);
+            StringBuilder dateBuilder = new StringBuilder(key);
+            int year = -1, month = -1, day = -1;
 
-                if (isDigit) {
-                    int digit = Character.digit(aChar, 10) * base;
-                    current += digit;
-                    base *= 10;
-                    isReading = true;
+            Matcher yearMatcher = YEAR_PATTERN.matcher(dateBuilder);
+            if (yearMatcher.find()) {
+                String yearString = yearMatcher
+                        .group();
+                year = Integer.parseInt(yearString);
+                dateBuilder.delete(yearMatcher.start(), yearMatcher.end());
+            }
+
+            Matcher monthMatcher = MONTH_PATTERN.matcher(dateBuilder);
+            if (monthMatcher.find()) {
+                String monthString = monthMatcher.group().toUpperCase();
+                month = MONTHS.get(monthString);
+                dateBuilder.delete(monthMatcher.start(), monthMatcher.end());
+            }
+
+            Matcher dayMatcher = DAY_PATTERN.matcher(dateBuilder);
+            while (dayMatcher.find()) {
+                int datePart = Integer.parseInt(dayMatcher.group());
+                if (month != -1) {
+                    day = datePart;
+                } else if (day == -1) {
+                    if (datePart > TOTAL_MONTHS) {
+                        day = datePart;
+                    } else {
+                        month = datePart;
+                    }
+                } else {
+                    break;
                 }
-
-                if ((!isDigit && isReading)) {
-                    dateParts.add(0, current);
-                    current = 0;
-                    base = 1;
-                    isReading = false;
-                }
             }
-
-            if (dateParts.size() != DEFAULT_DATE_PARTS) {
-                dateParts.add(0, current);
-            }
-
-            int first, second, third;
-            if (dateParts.size() == 1) {
-                int fullDate = dateParts.get(0);
-                first = fullDate % 100;
-                second = (fullDate / 100) % 100;
-                third = fullDate / 10000;
-            } else {
-                first = dateParts.get(0);
-                second = dateParts.get(1);
-                third = dateParts.get(2);
-            }
-
-            int year, month, day;
-            if (this.isDayOfMonth(first) && this.isMonth(second)) {
-                year = third;
-                month = second;
-                day = first;
-            } else if (this.isDayOfMonth(second) && this.isMonth(first)) {
-                year = third;
-                month = first;
-                day = second;
-            } else if (this.isDayOfMonth(second) && this.isMonth(third)) {
-                year = first;
-                month = third;
-                day = second;
-            } else if (this.isDayOfMonth(third) && this.isMonth(second)) {
-                year = first;
-                month = second;
-                day = third;
-            } else {
-                throw new DateTimeException("Invalid date: " + first + "-" + second + "-" + third);
-            }
+            
             return LocalDate.of(year, month, day);
         });
         return new PythonDate(result);
-    }
-
-    protected boolean isDayOfMonth(int second) {
-        return ChronoField.DAY_OF_MONTH.range().isValidValue(second);
-    }
-
-    protected boolean isMonth(int third) {
-        return ChronoField.MONTH_OF_YEAR.range().isValidValue(third);
     }
 
     @Override
--- bind/python-executor-bind-core/src/main/java/io/maksymuimanov/python/util/StringUtils.java
+++ bind/python-executor-bind-core/src/main/java/io/maksymuimanov/python/util/StringUtils.java
@@ -10,7 +10,7 @@ public class StringUtils {
     public final Pattern INT_PATTERN = Pattern.compile("^[+-]?[0-9]+$");
     public final Pattern FLOAT_PATTERN = Pattern.compile("^[+-]?[0-9]+[.]?[0-9]+$");
     public final Pattern BOOLEAN_PATTERN = Pattern.compile("^(True|False)$");
-    public final Pattern DATE_PATTERN = Pattern.compile("^([0-9]{4}\\D*[0-9]{2}\\D*[0-9]{2}|[0-9]{2}\\D*[0-9]{2}\\D*[0-9]{4})$");
+    public final Pattern DATE_PATTERN = Pattern.compile("^([0-9]{4}\\D*[0-9]{2}\\D*[0-9]{2}|[0-9]{2}\\D*[0-9]{2}\\D*[0-9]{4}|[0-9]{4}\\D*[A-Za-z]{3}\\D*[0-9]{2}|[0-9]{4}\\D*[0-9]{2}\\D*[A-Za-z]{3}|[0-9]{2}\\D*[A-Za-z]{3}\\D*[0-9]{4}|[A-Za-z]{3}\\D*[0-9]{2}\\D*[0-9]{4})$");
 
     public boolean isInteger(CharSequence value) {
         return isPattern(value, INT_PATTERN);
