c91bcb5b146857b50f140d78c0fd9b121d899ef8
Maksym Uimanov <uimanovmaks@gmail.com>
2026-01-21 18:31:49 +0100

    Remove AOP module due to its complexity, limitations and slowness

--- aop/jpyxie-spring-boot-aop-core/src/main/java/io/maksymuimanov/python/annotation/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package io.maksymuimanov.python.annotation;
-
-import org.jspecify.annotations.NullMarked;
--- aop/jpyxie-spring-boot-aop-core/src/main/java/io/maksymuimanov/python/aspect/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package io.maksymuimanov.python.aspect;
-
-import org.jspecify.annotations.NullMarked;
